36f835412f97c28db27ef7a525c35a6e65c56032
kk217 <kkc217@naver.com>
2022-06-09 23:19:56 +0900

    test: drag selection

--- public/js/explain_exercise3.js
+++ public/js/explain_exercise3.js
@@ -1,3 +1,4 @@
+
 let tmpStartPos = 0;
 let tmpLen = 0;
 
@@ -117,7 +118,7 @@ function handleCreateContextMenu_left(event){
     prevCtxMenu.remove();
   }
   
-  var selected = document.getSelection();
+  let selected = document.getSelection();
   // var selected = document.getSelection();
   if(selected.toString().length > 0) {
     highlightSelection(selected);
@@ -127,6 +128,8 @@ function handleCreateContextMenu_left(event){
   document.body.appendChild( ctxMenu );
 }
 
+let storedSelectStartPos = 0;
+let storedSelectLines = new Array();
 
 function highlightSelection(selected) {
   // console.log(selected);
@@ -156,63 +159,135 @@ function highlightSelection(selected) {
     selectionStartNumber = (startNum < endNum) ? startNum : endNum;
     selectionEndNumber = (startNum > endNum) ? startNum : endNum;
   }
+
+  // for (let i = 0; i < storedSelectLines.length; i++) {
+  //   let tbody = document.getElementById('left').children[0].children[0].children[0].children[0];
+  //   tbody.children[storedSelectStartPos + i].children[1].innerHTML = storedSelectLines[i];
+  // }
   
+  storedSelectLines = [];
+  storedSelectStartPos = selectionStartNumber - 1;
+
+
   selected = selected.getRangeAt(0);
+  // console.log(selected);
   let old = selected.cloneContents();
 
   if (selectionStartNumber != selectionEndNumber) {
-    let tbody = document.getElementById('left').children[0].children[0].children[0].children[0];x
-    let oldChildren = old.children;
-      
-    let frontSpan = createHilightedSpan(oldChildren[0].cells[0].innerHTML);
-    let frontInnerHTML = tbody.children[selectionStartNumber - 1].children[1].innerHTML;
-    let frontSelectStart = frontInnerHTML.lastIndexOf(oldChildren[0].cells[0].innerHTML);
-    frontInnerHTML = frontInnerHTML.slice(0, frontSelectStart) + frontSpan.innerHTML + frontInnerHTML.slice(frontSelectStart + oldChildren[0].cells[0].innerHTML.length);
-    tbody.children[selectionStartNumber - 1].children[1].innerHTML = frontInnerHTML;
-  
-    for (let i = 1; i < oldChildren.length - 1; i++) {
-      let span = createHilightedSpan(oldChildren[i].children[1].innerHTML);
-      tbody.children[selectionStartNumber + i - 1].children[1].innerHTML = span.innerHTML;
+    let safeRanges = getSafeRanges(selected);
+    for (let i = 0; i < safeRanges.length; i++) {
+      highlightRange(safeRanges[i]);
     }
+    // let tbody = document.getElementById('left').children[0].children[0].children[0].children[0];
+    // let oldChildren = old.children;
+    
+    // storedSelectLines.push(tbody.children[selectionStartNumber - 1].children[1].innerHTML);
+
+    // let frontSpan = createHilightedSpan(oldChildren[0].cells[0].innerHTML);
+    // let frontInnerHTML = tbody.children[selectionStartNumber - 1].children[1].innerHTML;
+    // let frontSelectStart = frontInnerHTML.lastIndexOf(oldChildren[0].cells[0].innerHTML);
+    // frontInnerHTML = frontInnerHTML.slice(0, frontSelectStart) + frontSpan.innerHTML + frontInnerHTML.slice(frontSelectStart + oldChildren[0].cells[0].innerHTML.length);
+    // tbody.children[selectionStartNumber - 1].children[1].innerHTML = frontInnerHTML;
+    
+    // for (let i = 1; i < oldChildren.length - 1; i++) {
+    //   storedSelectLines.push(tbody.children[selectionStartNumber + i - 1].children[1].innerHTML);
+    //   let span = createHilightedSpan(oldChildren[i].children[1].innerHTML);
+    //   tbody.children[selectionStartNumber + i - 1].children[1].innerHTML = span.innerHTML;
+    // }
+    
+    // storedSelectLines.push(tbody.children[selectionEndNumber - 1].children[1].innerHTML);
   
-    let backSpan = createHilightedSpan(oldChildren[oldChildren.length - 1].cells[1].innerHTML);
-    let backInnerHTML = tbody.children[selectionEndNumber - 1].children[1].innerHTML;
-    let backSelectStart = backInnerHTML.indexOf(oldChildren[oldChildren.length - 1].cells[1].innerHTML);
-    backInnerHTML = backInnerHTML.slice(0, backSelectStart) + backSpan.innerHTML + backInnerHTML.slice(backSelectStart + oldChildren[oldChildren.length - 1].cells[1].innerHTML.length);
-    tbody.children[selectionEndNumber - 1].children[1].innerHTML = backInnerHTML;
+    // let backSpan = createHilightedSpan(oldChildren[oldChildren.length - 1].cells[1].innerHTML);
+    // let backInnerHTML = tbody.children[selectionEndNumber - 1].children[1].innerHTML;
+    // let backSelectStart = backInnerHTML.indexOf(oldChildren[oldChildren.length - 1].cells[1].innerHTML);
+    // backInnerHTML = backInnerHTML.slice(0, backSelectStart) + backSpan.innerHTML + backInnerHTML.slice(backSelectStart + oldChildren[oldChildren.length - 1].cells[1].innerHTML.length);
+    // tbody.children[selectionEndNumber - 1].children[1].innerHTML = backInnerHTML;
+    // console.log(storedSelectLines);
   }
   else {
-    console.log(old);
-    // let span = createHilightedSpan();
+    let safeRanges = getSafeRanges(selected);
+    for (let i = 0; i < safeRanges.length; i++) {
+      highlightRange(safeRanges[i]);
+    }
   }
-  
-  // console.log(backSelectStart);
-  // tbody.children[selectionEndNumber - 1].children[1].innerHTML = frontInnerHTML;
-  // console.log(trs[0]);
 
-  // console.log(backRemainedText);
-  // backTr.children[1].innerHTML = '';
-  // console.log(backSpan);
-  // backTr.children[1].appendChild(backSpan);
-  // console.log(backSpan + backRemainedText);
-
-
-// 앞부분 추가 완료
-  // selected.deleteContents();
-  // tbody.children[selectionStartNumber - 1].children[1].appendChild(frontSpan);
-// ---------------------------------------------------------------
-
-
-  // tbody.children[selectionEndNumber - 1].children[0].remove();
-  // backTr.children[1].innerHTML = '';
-  // backTr.children[1].appendChild(backSpan);
-  // console.log(backRemainedText);
-  // backTr.children[1].innerText += backRemainedText;
-  // console.log(backTr);
-  // console.log(tbody.children[selectionEndNumber - 1]);
-  // for ()
-  // tbody.children[selectionEndNumber - 1].appendChild(backSpan);
 }
+
+function highlightRange(range) {
+  let newNode = document.createElement("div");
+  newNode.setAttribute(
+     "style",
+     "background-color: yellow; display: inline;"
+  );
+  range.surroundContents(newNode);
+}
+
+function getSafeRanges(dangerous) {
+  let a = dangerous.commonAncestorContainer;
+  // Starts -- Work inward from the start, selecting the largest safe range
+  let s = new Array(0), rs = new Array(0);
+  if (dangerous.startContainer != a)
+    for(let i = dangerous.startContainer; i != a; i = i.parentNode) {
+      s.push(i)
+    }
+  ;
+  if (0 < s.length) for(let i = 0; i < s.length; i++) {
+    let xs = document.createRange();
+    if (i) {
+        xs.setStartAfter(s[i-1]);
+        xs.setEndAfter(s[i].lastChild);
+    }
+    else {
+        xs.setStart(s[i], dangerous.startOffset);
+        xs.setEndAfter(
+            (s[i].nodeType == Node.TEXT_NODE)
+            ? s[i] : s[i].lastChild
+        );
+    }
+    rs.push(xs);
+  }
+
+  // Ends -- basically the same code reversed
+  let e = new Array(0), re = new Array(0);
+  if (dangerous.endContainer != a)
+    for(let i = dangerous.endContainer; i != a; i = i.parentNode)
+        e.push(i)
+  ;
+  if (0 < e.length) for(let i = 0; i < e.length; i++) {
+    let xe = document.createRange();
+    if (i) {
+        xe.setStartBefore(e[i].firstChild);
+        xe.setEndBefore(e[i-1]);
+    }
+    else {
+        xe.setStartBefore(
+            (e[i].nodeType == Node.TEXT_NODE)
+            ? e[i] : e[i].firstChild
+        );
+        xe.setEnd(e[i], dangerous.endOffset);
+    }
+    re.unshift(xe);
+  }
+
+  // Middle -- the uncaptured middle
+  
+  let xm = document.createRange();
+  if ((0 < s.length) && (0 < e.length)) {
+    xm.setStartAfter(s[s.length - 1]);
+    xm.setEndBefore(e[e.length - 1]);
+  }
+  else {
+    return [dangerous];
+  }
+
+  // Concat
+  rs.push(xm);
+  response = rs.concat(re);    
+
+  // Send to Console
+  return response;
+}
+
 
 function createHilightedSpan(text) {
   // let span = `<span id="selection" style="background-color:'#0FF0F0`;
